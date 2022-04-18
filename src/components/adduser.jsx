f1188bcc7e35bd9993b7188c31293319e6920f95
Sahilramjali <sahil.ramjali13@gmail.com>
2022-04-18 17:24:17 +0545

    added insert method

--- src/components/adduser.jsx
+++ src/components/adduser.jsx
@@ -1,17 +1,33 @@
-import { FormControl, FormGroup, Input, InputLabel,Button } from "@mui/material";
+import { FormControl, FormGroup, Input, InputLabel,Button, Typography } from "@mui/material";
+import { useState } from "react";
 import "../css/adduser.css";
+import { InsertUser } from "../services/api";
+const initialData={
+    name:'',
+    email:''
+}
 const AddUser=()=>{
+    const[user,setUser]=useState(initialData);
+  // const {name,email}=user;
+const onValueChange=(e)=>{
+    setUser({...user,[e.target.name]:e.target.value});
+   
+}
+const insertUserDetails=async()=>{
+    await InsertUser(user);
+}
     return(
        <FormGroup className="container">
-           <FormControl>
+           <Typography className="label">Add user</Typography>
+           <FormControl style={{margin:'5%'}}>
                <InputLabel>Name</InputLabel>
-               <Input/>
+               <Input onChange={(e)=>onValueChange(e)} name='name' className="enter"/>
            </FormControl>
-           <FormControl>
-               <InputLabel>Email</InputLabel>
-               <Input/>
+           <FormControl style={{margin:'5%'}}>
+               <InputLabel >Email</InputLabel>
+               <Input onChange={(e)=>onValueChange(e)} name='email' />
            </FormControl>
-           <Button>Insert</Button>
+           <Button variant="contained" color="success" onClick={insertUserDetails}>Insert</Button>
        </FormGroup>
     );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,3 +2,6 @@ import axios from "axios";
 export const fetchData=async()=>{
     return await axios.get("http://localhost:3333/Students");
 }
+export const InsertUser=async (user)=>{
+    return await axios.post("http://localhost:3333/Students",user);
+}
