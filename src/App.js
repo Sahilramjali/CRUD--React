5f1c5c8d7af60b631becadb0011594e76d3a6def
Sahilramjali <sahil.ramjali13@gmail.com>
2022-04-18 13:44:46 +0545

    installed necessary library

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
